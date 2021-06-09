5715ffcf4ae01155fb572f77409e5832b4f43852
“WesleyMeneghini” <wesley.meneghini@outlook.com>
2021-06-09 01:52:56 -0300

    Adicionando datas para pesquisa e melhorando a execuçao utilizando JS

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!DOCTYPE php>
 <html lang="pt-br">
 
 <head>
@@ -8,7 +8,6 @@
 
     <!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-
 
 
     <!--Let browser know website is optimized for mobile-->
@@ -28,17 +27,72 @@
             $('.modal').modal();
         });
 
-
         // Efeito para abrir e fechar as informaçoes do lançamento das comissoes
-        $(document).ready(function() {
-            $('.collapsible').collapsible({
-                accordion: false
+        const openCollapsible = () => {
+            $(document).ready(function() {
+                $('.collapsible').collapsible({
+                    accordion: false
+                });
             });
-        });
+        }
+
+        const relatorioPeriodo = (dataInicial, dataFinal) => {
+            $.ajax({
+                url: "relatorio.php",
+                type: 'GET',
+                data: {
+                    "data_inicial": dataInicial,
+                    "data_final": dataFinal
+                },
+                beforeSend: function() {
+                    $("#resultado_dias").html("Carregando Total dos dias do mês...");
+                }
+            }).done(function(msg) {
+                $("#resultado_dias").html(msg);
+
+                openCollapsible();
+            });
+        }
+
+        const totalOperadoras = (dataInicial, dataFinal) => {
+            $.ajax({
+                url: "total_operadoras.php",
+                type: 'GET',
+                data: {
+                    "data_inicial": dataInicial,
+                    "data_final": dataFinal
+                },
+                beforeSend: function() {
+                    $("#resultado").html("Carregando Total das Operadoras do mês...");
+                }
+            }).done(function(msg) {
+                let res = JSON.parse(msg);
+
+                let totalOperadoras = res.map(e => (
+                    `<tr>
+                        <td>${e.operadora}</td>
+                        <td> R$ ${e.total}</td>
+                    </tr>`)
+                );
+
+                let tableResultTotalOperadoras = 
+                        `<table>
+                            <thead>
+                                <tr>
+                                    <th>Operadora</th>
+                                    <th>Total</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                ${totalOperadoras}
+                            </tbody>
+                        </table>`;
+
+                $("#resultado_total_operadoras").html(tableResultTotalOperadoras);
+
+            });
+        }
     </script>
-
-
-
 
 
     <style>
@@ -52,25 +106,25 @@
 <body>
     <div class="container">
 
-        <form>
-            <div class="row">
-                <div class="input-field col s6 ">
-                    <input type="date" id="data_inicial" name="data_inicial" value="<?=date("Y-m-01")?>" />
+        <div class="row">
+            <form>
+                <div class="input-field col s5 ">
+                    <input type="date" id="data_inicial" name="data_inicial" value="<?= date("Y-m-01") ?>">
 
                     <label>Data inicial</label>
                 </div>
-                <div class="input-field col s6 ">
-                    <input type="date" id="data_final" name="data_final" value="<?=date("Y-m-d")?>"/>
+                <div class="input-field col s5 ">
+                    <input type="date" id="data_final" name="data_final" value="<?= date("Y-m-d") ?>">
                     <label>Data Final</label>
                 </div>
-                <button class="btn waves-effect waves-light" id="btn_pesquisa" type="submit" value="PESQUISAR">ENVIAR</button>
-
-            </div>
-        </form>
-
-        <div id="resultado_total_operadoras">
-
+                <div class="input-field col s2 ">
+                    <button class="btn waves-effect waves-light" id="btn_pesquisa" type="submit" value="PESQUISAR">ENVIAR</button>
+                </div>
+            </form>
         </div>
+
+        <div id="resultado_total_operadoras"></div>
+        <div id="resultado_dias"></div>
     </div>
 
     <script>
@@ -82,50 +136,9 @@
             if ($dataFinal < $dataInicial) {
                 alert("Data inicial não pode ser maior que a final!");
             } else {
-                $.ajax({
-                    url: "total_operadoras.php",
-                    type: 'POST',
-                    data: {
-                        "data_inicial": $dataInicial,
-                        "data_final": $dataFinal
-                    },
-                    beforeSend: function() {
-                        $("#resultado").html("Carregando Total das Operadoras do mês...");
-                    }
-                }).done(function(msg) {
-                    let res = JSON.parse(msg);
-
-                    let totalOperadoras = res.map(e => (
-                        `<tr>
-                            <td>${e.operadora}</td>
-                            <td> R$ ${e.total}</td>
-                        </tr>`));
-
-                    let tableResultTotalOperadoras = `
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th>Operadora</th>
-                                    <th>Total</th>
-                                </tr>
-                                </thead>
-                                ${totalOperadoras}
-                            </tbody>
-                        </table>`;
-                    
-                    $("#resultado_total_operadoras").html(tableResultTotalOperadoras);
-                        
-                })
+                relatorioPeriodo($dataInicial, $dataFinal);
+                totalOperadoras($dataInicial, $dataFinal);
             }
-
         });
     </script>
-
-    
-   
-    <?php
-    // require_once "./conferencia.php";
-    ?>
-
-    
 </body>
