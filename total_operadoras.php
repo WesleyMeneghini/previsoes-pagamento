237ba7455a45fdbf85e3b368d57c9582590893de
“WesleyMeneghini” <wesley.meneghini@outlook.com>
2021-06-17 16:12:34 -0300

    COrrigino o erro ao colocar os totais do previsto e pago

--- total_operadoras.php
+++ total_operadoras.php
@@ -11,7 +11,8 @@ $conect = conexaoMysql();
 $operadoras = array();
 $total = array();
 
-function formatMoeda($number){
+function formatMoeda($number)
+{
     return number_format($number, 2, ",", ".");
 }
 
@@ -26,46 +27,60 @@ if (isset($_GET['data_inicial']) && isset($_GET['data_final'])) {
     $dataInicial = $_GET['data_inicial'];
     $dataFinal = $_GET['data_final'];
 
-
     $pesquisaPorDatas = "'$dataInicial' AND '$dataFinal'";
 
-
     foreach ($operadoras as $operadora) {
+
         $idOperadora = $operadora['id'];
         $nomeOperadora = $operadora['titulo'];
 
+        $valorPagoPeriodoOperadora = 0;
+        $valorPrevistoPeriodoOperadora = 0;
+
         $sql = "SELECT 
-                sum(comissao) as comissao
-            FROM
-                busca_comissoes 
-            WHERE
-                (data_pagamento BETWEEN $pesquisaPorDatas)
-            AND id_operadora = $idOperadora;";
+                    sum(comissao) as comissao
+                FROM
+                    busca_comissoes 
+                WHERE
+                    (data_pagamento BETWEEN $pesquisaPorDatas)
+                AND id_operadora = $idOperadora;";
 
         $select = mysqli_query($conect, $sql);
 
-        if ($rs = mysqli_fetch_assoc($select)) {
+        if ($rsPago = mysqli_fetch_assoc($select)) {
+            $valorPagoPeriodoOperadora = $rsPago['comissao'];
+        }
+        
+        $sqlPrevisto = "SELECT 
+                            sum(valor) as valor_previsto 
+                        from 
+                            tbl_relatorio_recebimento 
+                        where 
+                            id_operadora = $idOperadora 
+                        and comissao = 0 AND (data BETWEEN $pesquisaPorDatas);";
 
-            $sqlPrevisto = "SELECT sum(valor) as valor_previsto from tbl_relatorio_recebimento where id_operadora = $idOperadora and comissao = 0 AND (data BETWEEN $pesquisaPorDatas);";
+        $selectPrevisto = mysqli_query($conect, $sqlPrevisto);
 
-            $selectPrevisto = mysqli_query($conect, $sqlPrevisto);
-            if ($rsPrevisto = mysqli_fetch_assoc($selectPrevisto)) {
-                $valorPrevisto = $rsPrevisto['valor_previsto'];
+        
+        if ($rsPrevisto = mysqli_fetch_assoc($selectPrevisto)) {
+            $valorPrevistoPeriodoOperadora = $rsPrevisto['valor_previsto'];
+        }
+
+        if ($valorPagoPeriodoOperadora > 0.0 || $valorPrevistoPeriodoOperadora > 0.0) {
+            
+            if ($valorPagoPeriodoOperadora == null) {
+                $valorPagoPeriodoOperadora = 0;
+            }
+            if ($valorPrevistoPeriodoOperadora == null) {
+                $valorPrevistoPeriodoOperadora = 0;
             }
 
-
-
-
-            $totalMesOperadora = $rs['comissao'];
-            if ($totalMesOperadora > 0.0) {
-                $totalOperadora = [
-                    'operadora' => $nomeOperadora, 
-                    'total' => $totalMesOperadora, 
-                    'previsto' => $valorPrevisto
-                ];
-                array_push($total, $totalOperadora);
-                // echo "$nomeOperadora => R$ $totalMesOperadora\n";
-            }
+            $totalOperadoraPeriodo = [
+                'operadora' => $nomeOperadora,
+                'total' => $valorPagoPeriodoOperadora,
+                'previsto' => $valorPrevistoPeriodoOperadora
+            ];
+            array_push($total, $totalOperadoraPeriodo);
         }
     }
 
